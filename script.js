c77183403ebe91f9442f8bf67e7ba0a545e2e649
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2023-11-28 23:12:35 -0400

    adding commit to revert code

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 $(function () {
-    // Display current day
+    // Display Current Day
     var currentDay = dayjs().format("dddd, MMMM D, YYYY");
     $("#currentDay").text(currentDay);
   
+    // Standard business hours (9 am to 5 pm)
     var workDayHours = Array.from({ length: 9 }, (_, index) => index + 9);
   
-    // Generating time blocks
+    // Generate timeblocks dynamically
     var timeBlockContainer = $(".container-lg");
-    workDayHours.forEach((hour, index) => {
+    workDayHours.forEach(hour => {
       var timeBlock = $("<div>").addClass("row time-block").attr("id", "hour-" + hour);
       timeBlock.append(
         $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(dayjs().hour(hour).format("hA")),
@@ -15,39 +16,35 @@ $(function () {
         $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").html('<i class="fas fa-save" aria-hidden="true"></i>')
       );
   
-      // Make all future event blocks green
+      // Apply "future" class to all time blocks
       timeBlock.addClass("future");
   
-      // Compare current hour and block hour ==> update class
+      // Compare current hour with time block hour and update class if needed
       var currentHour = dayjs().hour();
+      console.log("Current Hour:", currentHour, "Time Block Hour:", hour);
   
-      if (index < 0) {
-        var fixedText = "This text cannot be changed";
-        timeBlock.find(".description").val(fixedText).prop("disabled", true);
-      } else {
-        // Compare current hour with time block hour and update class if needed
-        if (hour < currentHour) {
-          timeBlock.addClass("past");
-        } else if (hour === currentHour) {
-          timeBlock.addClass("present");
-        } else {
-          timeBlock.addClass("future");
-        }
+      if (hour < currentHour) {
+        timeBlock.removeClass("future").addClass("past");
+      } else if (hour === currentHour) {
+        timeBlock.removeClass("future").addClass("present");
+      }
   
-        // Event listener for save button for events
-        timeBlock.find(".saveBtn").on("click", function () {
-          var eventText = timeBlock.find(".description").val();
-          var storageKey = "event-" + hour;
+      console.log("Classes After comparison:")
+
+
+      // Add event listener to save button for saving events to local storage
+      timeBlock.find(".saveBtn").on("click", function () {
+        var eventText = timeBlock.find(".description").val();
+        var storageKey = "event-" + hour;
+      
+        localStorage.setItem(storageKey, eventText);
+        console.log("Event saved for Hour " + hour);
+      });
   
-          localStorage.setItem(storageKey, eventText);
-          console.log("Event saved at " + hour);
-        });
-  
-        // Retrieve saved events and populate in time blocks
-        var storedEvent = localStorage.getItem("event-" + hour);
-        if (storedEvent) {
-          timeBlock.find(".description").val(storedEvent);
-        }
+      // Retrieve saved events from local storage and populate textareas
+      var storedEvent = localStorage.getItem("event-" + hour);
+      if (storedEvent) {
+        timeBlock.find(".description").val(storedEvent);
       }
   
       timeBlockContainer.append(timeBlock);
@@ -73,3 +70,4 @@ $(function () {
    //
    // TODO: Add code to display the current date in the header of the page.
 
+ 
