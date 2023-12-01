db6c6f8c1b73cb25a93add9d86ff87e48e5410b7
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2023-11-30 22:14:41 -0400

    added future if statemnt

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,13 @@ $(function () {
   var currentDay = dayjs().format("dddd, MMMM D, YYYY");
   $("#currentDay").text(currentDay);
 
-  // Standard business hours (9 am to 5 pm)
-  var workDayHours = Array.from({ length: 9 }, (_, index) => index + 9);
+  // Hours of the work day (9 am to 5 pm)
+  var workDayHours = Array.from({ length: 6 }, (_, index) => index + 12);;
 
-  // Generate timeblocks dynamically
+// Generate timeblocks 
   var timeBlockContainer = $(".container-lg");
+  var currentHour = dayjs().hour();
+
   workDayHours.forEach(hour => {
     var timeBlock = $("<div>").addClass("row time-block").attr("id", "hour-" + hour);
     timeBlock.append(
@@ -16,59 +18,39 @@ $(function () {
       $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").html('<i class="fas fa-save" aria-hidden="true"></i>')
     );
 
-    // Apply "future" class to all time blocks
-    timeBlock.addClass("future");
-
-    // Compare current hour with time block hour and update class if needed
-    var currentHour = dayjs().hour();
-    console.log("Current Hour:", currentHour, "Time Block Hour:", hour);
-
+    // Compare current hour + timeBlock --> update if needed 
     if (hour < currentHour) {
       timeBlock.removeClass("future").addClass("past");
     } else if (hour === currentHour) {
       timeBlock.removeClass("future").addClass("present");
     }
 
-    console.log("Classes After comparison:")
-
-
     // Add event listener to save button for saving events to local storage
     timeBlock.find(".saveBtn").on("click", function () {
       var eventText = timeBlock.find(".description").val();
       var storageKey = "event-" + hour;
-    
+      console.log("Save button clicked")
+
       localStorage.setItem(storageKey, eventText);
       console.log("Event saved for Hour " + hour);
     });
 
-    // Retrieve saved events from local storage and populate textareas
+    // Retrieve saved events from local storage and idsplay in timeBlock 
     var storedEvent = localStorage.getItem("event-" + hour);
     if (storedEvent) {
       timeBlock.find(".description").val(storedEvent);
     }
 
+    //set timeBlocks green 
+    if (hour > 11) {
+      timeBlock.removeClass("past present").addClass("future");
+    }
+
+
     timeBlockContainer.append(timeBlock);
   });
 });
 
-  
-   // TODO: Add a listener for click events on the save button. This code should
-   // use the id in the containing time-block as a key to save the user input in
-   // local storage. HINT: What does `this` reference in the click listener
-   // function? How can DOM traversal be used to get the "hour-x" id of the
-   // time-block containing the button that was clicked? How might the id be
-   // useful when saving the description in local storage?
-   //
-   // TODO: Add code to apply the past, present, or future class to each time
-   // block by comparing the id to the current hour. HINTS: How can the id
-   // attribute of each time-block be used to conditionally add or remove the
-   // past, present, and future classes? How can Day.js be used to get the
-   // current hour in 24-hour time?
-   //
-   // TODO: Add code to get any user input that was saved in localStorage and set
-   // the values of the corresponding textarea elements. HINT: How can the id
-   // attribute of each time-block be used to do this?
-   //
-   // TODO: Add code to display the current date in the header of the page.
 
+ 
  
